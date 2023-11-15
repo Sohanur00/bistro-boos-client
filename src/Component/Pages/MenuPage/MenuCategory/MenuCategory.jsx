d5e631db18435630658d78e5e5d46504d35b5363
Sohanur00 <sk1882753729@gmail.com>
2023-11-16 01:15:58 +0600

    cetagory aded

--- src/Component/Pages/MenuPage/MenuCategory/MenuCategory.jsx
+++ src/Component/Pages/MenuPage/MenuCategory/MenuCategory.jsx
@@ -1,10 +1,24 @@
+/* eslint-disable react/prop-types */
+import Cover from "../../shared/Cover";
+import MenuItem from "../../shared/MenuItem";
 
 
-const MenuCategory = () => {
-
-
-
-
+const MenuCategory = ({items,title,Img}) => {
+    return (
+        <div className="pt-8">
+            {
+              title &&  <Cover img={Img} title={"OUR MENU"} ></Cover>
+            }
+              
+             <div className="grid md:grid-cols-2 gap-10 mb-8">
+                    {
+                        items.map(item =><MenuItem key={item._id} item={item}></MenuItem> )
+                    } 
+                     
+                    
+                </div>
+        </div>
+    );
 };
 
 export default MenuCategory;
--- src/Component/Pages/MenuPage/MenuPage.jsx
+++ src/Component/Pages/MenuPage/MenuPage.jsx
@@ -1,9 +1,21 @@
+/* eslint-disable no-unused-vars */
 import { Helmet } from 'react-helmet-async';
 import Cover from '../shared/Cover';
 import menuImage from '../../../assets/menu/banner3.jpg'
-import PopularMenu from '../Home/PopularMenu';
+import dessertImg from '../../../assets/menu/dessert-bg.jpeg'
+
+import useMenu from '../../Hooks/useMenu';
+import SectionTitle from '../../SectionTitle/SectionTitle';
+import MenuCategory from './MenuCategory/MenuCategory';
 
 const MenuPage = () => {
+const [menu] = useMenu();
+const desserts = menu.filter(item => item.category === 'dessert')
+const soup = menu.filter(item => item.category === 'soup')
+const salad = menu.filter(item => item.category === 'salad')
+const pizza = menu.filter(item => item.category === 'pizza')
+const offered = menu.filter(item => item.category === 'offered')
+
     return (
         <div>
             <Helmet>
@@ -11,11 +23,25 @@ const MenuPage = () => {
                 <link rel="canonical" href="https://www.tacobell.com/" />
             </Helmet>
             <Cover img={menuImage} title={"OUR MENU"} ></Cover>
-            <PopularMenu></PopularMenu>
-            <Cover img={menuImage} title={"OUR MENU"} ></Cover>
-            <PopularMenu></PopularMenu>
-            <Cover img={menuImage} title={"OUR MENU"} ></Cover>
-            <PopularMenu></PopularMenu>
+            {/* main cover */}
+           <SectionTitle
+           
+           subHeading={"---Don't miss---"}
+           heading={"TODAY'S OFFER"}
+           
+           >
+            {/* 
+            
+            offered menu item
+            */}
+
+           </SectionTitle>
+           <MenuCategory items={offered}> </MenuCategory>
+           {/* dessert menu items */}
+           <MenuCategory items={desserts}
+           title="Dessert"
+           Img={dessertImg}
+           ></MenuCategory>
         </div>
     )
 };
